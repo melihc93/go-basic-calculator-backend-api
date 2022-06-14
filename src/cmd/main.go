c42e7e1e8554c324d28282748d8f1564f2e3b20e
melihc93 <melihc10@gmail.com>
2022-06-15 00:56:24 +0300

    main method minor changes

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,56 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"go-basic-calculator-backend-api/src/endpointhandlers"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
-
-type responseObject struct {
-	Result string `json:"result"`
-	User   string `json:"user"`
-}
-
-type errorObject struct {
-	Message string `json:"message"`
-}
-
-var responseObjects responseObject
-var errorObjects errorObject
-
-func sumResponse(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("sum response object creating...")
-	query := r.URL.Query()
-	fmt.Println(query["params"])
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-}
-
-func addResponse(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("add response object creating...")
-	query := r.URL.Query()
-	fmt.Println(query["params"])
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-}
-
-func subsResponse(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("subtraction response object creating...")
-	query := r.URL.Query()
-	fmt.Println(query["params"])
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-}
-
-func multResponse(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("multiplication response object creating...")
-	query := r.URL.Query()
-	fmt.Println(query["params"])
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-}
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
